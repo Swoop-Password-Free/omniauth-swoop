0c55660595a070b2c97c60f8a2be0e6fb90af914
charlie763 <cwisoff@gmail.com>
2020-12-03 16:18:38 -0700

    first stab at writing strategy

--- lib/omniauth/strategies/swoop.rb
+++ lib/omniauth/strategies/swoop.rb
@@ -3,7 +3,24 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Swoop < OmniAuth::Strategies::OAuth2
-    
+      option :client_options, {
+        site: 'https://auth.swoop.email', 
+        token_url: '/oauth2/token',
+        user_info_url: '/oauth2/profile'
+      }
+
+      option :user_response_structure, {
+        attributes: {
+          email: 'email',
+          uid: 'sub'
+        }
+      }
+
+      option :authorize_params, {
+          scope: 'email'
+      }
+
+      option :name, "swoop"
     end
   end
 end
